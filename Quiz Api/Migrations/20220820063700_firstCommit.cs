d0b5c6f2608cec23d8136ef32ff0e429bcc7a5d8
Ranjithkumar-Karuppaiya <94510835+Ranjithkumar-Karuppaiya@users.noreply.github.com>
2022-09-01 15:35:06 +0530

    first commit

---
+++ Quiz Api/Migrations/20220820063700_firstCommit.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quiz_Api.Migrations
+{
+    public partial class firstCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    ParticipantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Score = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    TimeTaken = table.Column<string>(type: "nvarchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.ParticipantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    QId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionInWords = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    ImageName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Option1 = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Option2 = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Option3 = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Option4 = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
---
+++ Quiz Api/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Quiz_Api.Models
+{
+    public class Question
+    {
+        [Key]
+        public int QId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? QuestionInWords { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string? ImageName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Option1  { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Option2  { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Option3 { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Option4 { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public int Answer { get; set; }
+    }
+}
